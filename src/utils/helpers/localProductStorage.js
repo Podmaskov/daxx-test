114ebfd37e0bd53a5b0389f25fb793345d7a8eaf
Artem Podmaskov <podmaskov@gmail.com>
2020-01-25 11:10:05 +0200

    create localProductStorage

---
+++ src/utils/helpers/localProductStorage.js
@@ -0,0 +1,47 @@
+
+import uuidv4 from 'uuid/v4'
+
+export default class LocalProductStorage {
+  constructor() {
+    this.localStorage = window.localStorage
+  }
+
+  setProduct(product) {
+    const products = this.localStorage.getItem(products) || []
+    const productWithId = {
+      ...product,
+      id: uuidv4()
+    }
+    const productsToSave = [productWithId, ...products]
+    this.localStorage.setItem('products', JSON.stringify(productsToSave))
+  }
+
+  getProducts() {
+    return JSON.parse(this.localStorage.getItem('products')) || []
+  }
+
+  updateProduct(newProduct) {
+    const products = this.localStorage.getItem(products) || []
+    const updatedProducts = products.map((product) => {
+      if (newProduct.id === product.id) {
+        return {
+          ...newProduct
+        }
+      }
+      return product
+    })
+    this.localStorage.setItem('products', JSON.stringify(updatedProducts))
+  }
+
+  deleteProduct(productId){
+    const products = this.localStorage.getItem(products) || []
+    const productsToSave = products.filter( (product) => {
+      if(product.id === productId) return
+      return {
+        ...product
+      }
+    })
+    this.localStorage.setItem('products', JSON.stringify(productsToSave))
+  }
+}
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,9 @@
 </template>
 
 <script>
-export default {
 
+
+export default {
 }
 </script>
 
